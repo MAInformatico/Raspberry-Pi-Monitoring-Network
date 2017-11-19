3aeb29b6be64eea2f00d43e7b8ab5fd2f8384e4f
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2017-11-19 13:36:17 +0100

    Add files via upload

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,9 @@
-sudo rm monitor.txt;
-sudo nmap -sP LAN_IP/24 > monitor.txt;
-sudo mpack -s "Equipos conectados" /root/monitor.txt your_email@email.com
+sudo nmap -sP 192.168.1.0/24 > monitor.txt;
+sudo mpack -s "Connected hosts" /root/monitor.txt email@email.com;
+sudo /opt/vc/bin/vcgencmd measure_temp > temperature.txt;
+sudo mpack -s "Temperature server" /root/temperature.txt email@email.com;
+sudo ps -aux > process.txt;
+sudo mpack -s "Procesos activos" /root/process.txt email@email.com;
+sudo netstat -natp > ports.txt
+sudo mpack -s "Active ports" /root/ports.txt email@email.com;
+rm *.txt
