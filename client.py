63eef268ec66c45b42aa030903ee56a4dff582e0
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-01-13 18:28:40 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import urllib.request, urllib.error, urllib.parse
 
 Files = ['temperature.txt','ports.txt','process.txt','hosts.txt']
 Port = '1234'
-IP = 'your_IP'
+IP = 'your_RasberryPi_IP'
 
 for i in range(len(Files)):
     url = 'http://'+ str(IP)+ ':' + str(Port) + '/' + str(Files[i])
