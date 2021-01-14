fa0d2ba484b6c66cdc90fcc6e03cd0629065d9d7
Mike <magutierrezsistemas@gmail.com>
2021-01-14 08:35:28 +0100

    better file organization

--- RaspberryPiFiles/sniff.sh
+++ RaspberryPiFiles/sniff.sh
@@ -1,2 +1,1 @@
 sudo touch today$(date +%d%m%y).pcap && sudo tshark -nni eth0 -a duration:86400  -w today$(date +%d%m%y).pcap && tar -cvf comprimed.tar today$(date +%d%m%y).pcap
-echo "Everything OK" | mail -s "Success" <your-email>
