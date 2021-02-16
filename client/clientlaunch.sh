88f9cf39f91146a6219619f7688520a8518b7b51
Mike <magutierrezsistemas@gmail.com>
2021-02-16 06:48:06 +0100

    changing launcher

--- client/clientlaunch.sh
+++ client/clientlaunch.sh
@@ -1,2 +1,6 @@
-#You can use it to schedule a cron task
-python3 client.py
+
+if java -version 2>&1 >/dev/null | grep "java version\|openjdk version" ; then
+	javac client.java; java client
+else
+	[[ "$(python3 -V)" =~ "Python 3" ]] && python3 client.py
+fi
