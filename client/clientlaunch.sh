d2e85c91f9fed8deb7e5790b477bb9c76b869281
Mike <magutierrezsistemas@gmail.com>
2021-02-22 06:50:24 +0100

    updating client launcher

--- client/clientlaunch.sh
+++ client/clientlaunch.sh
@@ -1,5 +1,7 @@
+cd /home/mike/Descargas/RasPiFileReceived;
 
 if java -version 2>&1 >/dev/null | grep "java version\|openjdk version" ; then
+	cd javaclient
 	javac client.java; java client
 else
 	pythonver=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
