507ee7386ecbdcd6bbcb05497993956ee3a97601
Mike <magutierrezsistemas@gmail.com>
2021-02-28 14:15:38 +0100

    removing emails dependencies

--- RaspberryPiFiles/monitor.sh
+++ RaspberryPiFiles/monitor.sh
@@ -1,12 +1,7 @@
 sudo nmap -sP 192.168.1.0/24 > hosts.txt;
-#sudo mpack -s "Current hosts" /root/hosts.txt magutierrezsistemas@gmail.com;
 sudo /opt/vc/bin/vcgencmd measure_temp > temperature.txt;
-#sudo mpack -s "Server's temperature" /root/temperature.txt magutierrezsistemas@gmail.com;
 sudo ps -aux > process.txt;
-#sudo mpack -s "Current process" /root/process.txt magutierrezsistemas@gmail.com;
 sudo netstat -natp > ports.txt;
-#sudo mpack -s "Listening Ports" /root/ports.txt magutierrezsistemas@gmail.com;
-#rm *.txt
 date >> HTTPsConnections.txt;
 if ping -c 1 192.168.1.255 &> /dev/null #put here the IP of your client
 then
